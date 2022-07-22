bced57f94760ef1ffe04a1f50af96b4b7538bd19
Apta <109815096+Apta-tea@users.noreply.github.com>
2022-07-22 23:37:45 +0700

    Create AboutBox1.vb

---
+++ AboutBox1.vb
@@ -0,0 +1,12 @@
+Public NotInheritable Class AboutBox1
+
+    Private Sub AboutBox1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Set the title of the form.
+        Me.Text = "Aplikasi Presensi Ver-1.0"
+    End Sub
+
+    Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
+        Me.Close()
+    End Sub
+
+End Class
